98a8fee4d5bae29a3b885ffc72e1df227ddb31af
GPL9056 <rrr900506@gmail.com>
2022-07-19 02:57:01 +0800

    modal-project

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -31,118 +31,10 @@
         <b-card no-body class="left_down">
           <b-tabs card>
             <b-tab title="評分" active>
-              <span style="text-align:left; font-weight:bold; float:left">加權調整</span>
-              <span style="text-align:left; font-weight:bold; float:right">評分</span><br><br>
-              
-              <div style="height:200px; overflow:auto">
-              <div v-if="jobs_selected === 'a'"><!--切換行業-->
-                <div style="width:300px; text-align:center;"><!--幼兒園-->
-                  <div style="width:auto; *width:100px; margin:0 auto; display:inline-block;">
-                      <span style="width:auto; display:block; text-align:center;">公園
-                        <input type="checkbox" style="width:20px; height:20px; float:left">
-                      </span>
-                      <label for="customRange1" class="form-label">{{scorepark}}%</label><!--可任意調分-->
-                      <input type="range" class="form-range" id="customRange1" v-model="scorepark"><br><br>
-                      <span style="width:auto; display:block; text-align:center;">捷運站
-                        <input type="checkbox" style="width:20px; height:20px; float:left">
-                      </span>
-                      <label for="customRange1" class="form-label">{{scoremrtone}}%</label>
-                      <input type="range" class="form-range" id="customRange1" v-model="scoremrtone"><br><br>
-                      <span style="width:auto; display:block; text-align:center;">租金
-                        <input type="checkbox" style="width:20px; height:20px; float:left">
-                      </span>
-                      <label for="customRange1" class="form-label">{{scoremoneyone}}%</label>
-                      <input type="range" class="form-range" id="customRange1" v-model="scoremoneyone"><br><br>
-                  </div>
-                </div>
-              </div>
-              <div v-else-if="jobs_selected === 'b'">
-                <div style="width:300px; text-align:center;"><!--機車行-->
-                  <div style="width:auto; *width:100px; margin:0 auto; display:inline-block;">
-                      <span style="width:auto; display:block; text-align:center;">市場
-                        <input type="checkbox" style="width:20px; height:20px; float:left">
-                      </span>
-                      <label for="customRange1" class="form-label">{{scoremarket}}%</label>
-                      <input type="range" class="form-range" id="customRange1" v-model="scoremarket">
-                      <div style="width:300px; text-align:center;"><!--市場細項-->
-                        <div style="width:auto; *width:100px; margin:0 auto; display:inline-block;">
-                            <span style="width:auto; display:block; text-align:center;">小北
-                              <input type="checkbox" style="width:20px; height:20px; float:left">
-                            </span>
-                            <span style="width:auto; display:block; text-align:center;">全聯
-                              <input type="checkbox" style="width:20px; height:20px; float:left">
-                            </span>
-                            <span style="width:auto; display:block; text-align:center;">美廉社
-                              <input type="checkbox" style="width:20px; height:20px; float:left">
-                            </span><br>
-                        </div>
-                      </div>
-                      <span style="width:auto; display:block; text-align:center;">捷運站
-                        <input type="checkbox" style="width:20px; height:20px; float:left">
-                      </span>
-                      <label for="customRange1" class="form-label">{{scoremrttwo}}%</label>
-                      <input type="range" class="form-range" id="customRange1" v-model="scoremrttwo"><br><br>
-                      <span style="width:auto; display:block; text-align:center;">租金
-                        <input type="checkbox" style="width:20px; height:20px; float:left">
-                      </span>
-                      <label for="customRange1" class="form-label">{{scoremoneytwo}}%</label>
-                      <input type="range" class="form-range" id="customRange1" v-model="scoremoneytwo"><br><br>
-                  </div>
-                </div>
-              </div>
-              <div v-else>
-                <div style="width:300px; text-align:center;"><!--健身房-->
-                  <div style="width:auto; *width:100px; margin:0 auto; display:inline-block;">
-                      <span style="width:auto; display:block; text-align:center;">百貨公司
-                        <input type="checkbox" style="width:20px; height:20px; float:left">
-                      </span>
-                      <label for="customRange1" class="form-label">{{scorede}}%</label>
-                      <input type="range" class="form-range" id="customRange1" v-model="scorede"><br><br>
-                      <!--<div style="width:300px; text-align:center;">
-                        <div style="width:auto; *width:100px; margin:0 auto; display:inline-block;">
-                          <span style="width:auto; display:block; text-align:center;">大遠百</span>
-                          <span style="width:auto; display:block; text-align:center;">新光三越</span>
-                          <span style="width:auto; display:block; text-align:center;">SOGO</span>
-                         </div>
-                      </div>-->
-                      <span style="width:auto; display:block; text-align:center;">捷運站
-                        <input type="checkbox" style="width:20px; height:20px; float:left">
-                      </span>
-                      <label for="customRange1" class="form-label">{{scoremrtthree}}%</label>
-                      <input type="range" class="form-range" id="customRange1" v-model="scoremrtthree"><br><br>
-                      <span style="width:auto; display:block; text-align:center;">租金
-                        <input type="checkbox" style="width:20px; height:20px; float:left">
-                      </span>
-                      <label for="customRange1" class="form-label">{{scoremoneythree}}%</label>
-                      <input type="range" class="form-range" id="customRange1" v-model="scoremoneythree"><br><br>
-                  </div>
-                </div>
-              </div>
-              </div>
               <!-- 加權條整區modal, 參數為行業 -->
               <!-- <Score :jobType="jobs" :distance="distance"/> -->
             </b-tab>
             <b-tab title="總覽">
-              <div style="height:275px; overflow:auto">
-                <p>地點資訊</p>
-                <!--<div class="tableline">-->
-                  <p style="text-align:left">預估需求：</p>
-                  <p></p>
-                  <p style="text-align:left">租金：</p>
-                  <p></p>
-                  <p style="text-align:left">地址：</p>
-                  <p></p>
-                  <p style="text-align:left">地址：</p>
-                  <p></p>
-                  <p style="text-align:left">附近競爭者數：</p>
-                  <p></p>
-                  <p style="text-align:left">附近市場數：</p>
-                  <p></p>
-                  <p style="text-align:left">附近捷運站數：</p>
-                  <p></p>
-                  <p style="text-align:left">區域平均租金：</p>
-                  <p></p>
-                <!--</div>-->
               </div>
               <!-- 參數...好像...很多... -->
               <!-- <Overview :jobType="jobs" :distance="distance"/> -->
@@ -203,8 +95,7 @@ export default {
       scoremoneytwo:50,
       scorede:50,
       scoremrtthree:50,
-      scoremoneythree:50,
-      score:50
+      scoremoneythree:50
     }
   }
 }
@@ -270,13 +161,6 @@ export default {
 .sizw-check{
   width: 30px;
   height: 30px;
-}
-.le{
-  text-align:left;
-}
-[type="checkbox"] {
-  display: grid;
-  place-content: center;
 }
 .dis_range{
   width: 160px;
--- modal-project/src/components/Overview.vue
+++ modal-project/src/components/Overview.vue
@@ -1,4 +1,22 @@
 <template>
+    <div style="height:275px; overflow:auto">
+        <p>地點資訊</p>
+        <!--<div class="tableline">-->
+          <p style="text-align:left">預估需求：</p>
+          <p></p>
+          <p style="text-align:left">租金：</p>
+          <p></p>
+          <p style="text-align:left">地址：</p>
+          <p></p>
+          <p style="text-align:left">附近競爭者數：</p>
+          <p></p>
+          <p style="text-align:left">附近市場數：</p>
+          <p></p>
+          <p style="text-align:left">附近捷運站數：</p>
+          <p></p>
+          <p style="text-align:left">區域平均租金：</p>
+          <p></p>
+        <!--</div>-->
 </template>
 
 <script>
@@ -10,4 +28,7 @@ export default {
 </script>
 
 <style scoped>
+.le{
+  text-align:left;
+}
 </style>
